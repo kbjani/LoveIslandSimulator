809e17dbc0d8a00e08a4a35337537b3ca589d9a9
adavidson21 <alexis.davidson221@gmail.com>
2024-11-04 23:37:32 -0500

    Add home view

--- src/main/java/com/example/loveislandsimulator/HomeController.java
+++ src/main/java/com/example/loveislandsimulator/HomeController.java
@@ -3,7 +3,7 @@ package com.example.loveislandsimulator;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class HomeController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/com/example/loveislandsimulator/LoveIslandSimulatorApp.java
+++ src/main/java/com/example/loveislandsimulator/LoveIslandSimulatorApp.java
@@ -7,13 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class LoveIslandSimulatorApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LoveIslandSimulatorApp.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Love Island Game Show Simulator");
         stage.setScene(scene);
+
+        stage.setWidth(1024);
+        stage.setHeight(768);
+
         stage.show();
     }
 
