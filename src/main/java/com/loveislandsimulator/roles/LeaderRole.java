8292a26e84c728fb10b416a244eeba363f468333
kbjani <156864397+kbjani@users.noreply.github.com>
2024-11-28 15:11:16 -0600

    Update LeaderRole.java

--- src/main/java/com/loveislandsimulator/roles/LeaderRole.java
+++ src/main/java/com/loveislandsimulator/roles/LeaderRole.java
@@ -6,14 +6,16 @@ import com.loveislandsimulator.models.ChallengeCommand;
 import com.loveislandsimulator.models.Islander;
 
 public class LeaderRole extends RoleDecorator {
-
-    public LeaderRole(Islander islander) {
-        super(islander, Role.LEADER);
+    
+    public LeaderRole(Contestant islander) {
+        super(islander);
     }
 
     @Override
-    public void participateInChallenge(ChallengeCommand challenge) {
-        super.participateInChallenge(challenge);
-        System.out.println(islander.getName() + ", " + Role.getRoleName(Role.LEADER));
+    public void addPoints(double points) {
+        double wrappedPoints = points + 10;
+        super.addPoints(wrappedPoints);
+        System.out.println(this.wrappedIslander.getName() + " applied Leader role for 10 extra points...");
     }
+
 }
